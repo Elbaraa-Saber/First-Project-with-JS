c2e66b103a0903ff678a224bd73c78cf6e98aa1b
Eblaraa <112890110+Baraa-Saber@users.noreply.github.com>
2024-01-23 00:34:44 +0700

    change background image every 5 sec

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+// First way to change photo every five seconds:
+
 // Selet Random pager element 
 let landingPage = document.querySelector(".landing-page");
 // Get images 
@@ -5,6 +7,11 @@ let imgsArray = ["01.jpg" , "02.jpg" ,"03.jpg" ,"04.jpg" ,"05.jpg" ];
 // Function to change img 
 setInterval(() => {
     let randomNumber = Math.floor(Math.random() * imgsArray.length);
-    landingPage.style.backgroundImage = 'url("imeges/'+ imgsArray[randomNumber] +'")'
-} , 1000);
+    landingPage.style.backgroundImage = 'url("imeges/'+ imgsArray[randomNumber] +'")';
+} , 5000);
 
+// // Second way to change photo every five seconds:
+// setInterval(() => {
+//     let randomNumber = Math.floor(Math.random() * 5);
+//     document.querySelector(".landing-page").style.backgroundImage = 'url("imeges/0'+ randomNumber +'.jpg")';
+// } , 5000);

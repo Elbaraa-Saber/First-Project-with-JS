bf7d06023944d1001f85b6b2452ac7a7f881fcaa
Eblaraa <112890110+Baraa-Saber@users.noreply.github.com>
2024-02-01 22:21:35 +0700

    ended all editing in color page

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,15 @@
+// How to charge element in localStorage 
+let mainColors = localStorage.getItem("color-option");
+if(mainColors !== null){
+    document.documentElement.style.setProperty("--main-color", mainColors);
+    document.querySelectorAll(".colors-list li").forEach(element => {
+        element.classList.remove("active");
+        if(mainColors === element.dataset.color){
+            element.classList.add("active");
+        }
+    });
+}
+
 //Add spin to gear icon
 document.querySelector(".setting-box .toggle-settings i").onclick = function (){
     // Add fa-spiin for rotation on self
@@ -5,14 +17,34 @@ document.querySelector(".setting-box .toggle-settings i").onclick = function (){
     // Add open class for setting-box for open this element in screen
     document.querySelector(".setting-box").classList.toggle("open");
 }
+
 // Switch colors 
 const colorsLi = document.querySelectorAll(".colors-list li");
 // Loop 
 colorsLi.forEach(li => {
-    li.addEventListener("click" , (e) => {
+    li.addEventListener("click" , (e) => { 
         document.documentElement.style.setProperty("--main-color" , e.target.dataset.color);
+        // Add color in localStorage 
+        localStorage.setItem("color-option" , e.target.dataset.color );
+        // Remove all active class from all elements 
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active");
+        });
+        // add active class for choisen element 
+        e.target.classList.add("active");
     });
 });
+
+// switch random background option to choisen element 
+const randomBackEl = document.querySelectorAll(".setting-container .random-background span");
+randomBackEl.forEach(span => {
+    span.addEventListener("click" , (e) => {
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active");
+        })
+        e.target.classList.add("active");
+    })
+})
 
 // First way to change photo every five seconds:
 // Selet Random pager element 
